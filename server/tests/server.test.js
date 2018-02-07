7900cb4c0576cae34cbf094a2e4a18ec525f4523
Zack Tzeng <zack.tzeng@gmail.com>
2018-02-07 17:33:24 -0600

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,60 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+// beforeeach() takes a function which has done as its argument
+// beforeeach is called before each test is run
+	// can be used to setup test database
+beforeEach((done) => {
+	// this removes the collection to ensure the tests after can work properly
+	Todo.remove({}).then(() => done());
+})
+
+describe('POST /todos', () => {
+	it('should create a new todo', (done) => {
+		var text = 'Test todo text';
+
+		request(app)
+			.post('/todos')
+			.send({text})
+			.expect(200)
+			// this is a custom expect assertion
+			// it takes in a function with res as its argument
+			.expect((res) => {
+				expect(res.body.text).toBe(text);
+			})
+			// this check if there is any error during the assertion
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				// this Todo is a model object so its find method return everything document if successful
+				Todo.find().then((todos) => {
+					expect(todos.length).toBe(1);
+					expect(todos[0].text).toBe(text);
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+
+	it('should not create to with invalid body data', (done) => {
+		var text = '   ';
+
+		request(app)
+			.post('/todos')
+			.send({text})
+			.expect(400)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				Todo.find().then((todos) => {
+					expect(todos.length).toBe(0);
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+});
