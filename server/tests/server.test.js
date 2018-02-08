f412c196c89517526e70e7f34cf9567134a51e8a
Zack Tzeng <zack.tzeng@gmail.com>
2018-02-07 18:13:46 -0600

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,13 +4,21 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
+// we need a dummy array of todos for testing GET /todos because we assume the database isn't empty
+const todos = [{text: 'Postman task1'}, {text: 'Postman task2'}, {text: 'Postman task3'}, {text: 'Postman task4'}];
+
+
+
 // beforeeach() takes a function which has done as its argument
 // beforeeach is called before each test is run
 	// can be used to setup test database
 beforeEach((done) => {
 	// this removes the collection to ensure the tests after can work properly
-	Todo.remove({}).then(() => done());
-})
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);	
+	}).then(() => done());
+});
 
 describe('POST /todos', () => {
 	it('should create a new todo', (done) => {
@@ -31,7 +39,7 @@ describe('POST /todos', () => {
 					return done(err);
 				}
 				// this Todo is a model object so its find method return everything document if successful
-				Todo.find().then((todos) => {
+				Todo.find({text}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -52,9 +60,23 @@ describe('POST /todos', () => {
 				}
 
 				Todo.find().then((todos) => {
-					expect(todos.length).toBe(0);
+					expect(todos.length).toBe(4);
 					done();
 				}).catch((e) => done(e));
 			});
+	});
+
+});
+
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(4);
+			})
+			// we don't need to have error handling ability for get because we are just reading what's in the db
+			.end(done);
 	});
 });
