886875ab5e34a2255a3ecc4d182d607c0aa302bb
Zack Tzeng <zack.tzeng@gmail.com>
2018-02-08 11:04:56 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose.js');
@@ -30,6 +31,21 @@ app.get('/todos', (req, res) => {
 	}, (err) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			res.status(404).send();
+		}
+		res.status(200).send(todo);
+	}, (e) => {
+		res.status(400).send();
+	}).catch((e) => {res.status(400).send()});
 });
 
 
