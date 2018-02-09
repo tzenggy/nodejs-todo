74095ce50b4c48ee83eedcf11c969e58666f3e52
Zack Tzeng <zack.tzeng@gmail.com>
2018-02-09 10:14:05 -0600

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 
 var {mongoose} = require('./db/mongoose.js');
@@ -65,6 +66,35 @@ app.delete('/todos/:id', (req, res) => {
 		return res.status(400).send("error");
 	} don't need the second error handling, simply use catch */).catch((e) => {
 		res.status(400).send("error");
+	});
+});
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {
+		$set: body
+	}, {
+		new: true
+	}).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.status(200).send({todo});
+	}).catch((e) => {
+		res.status(400).send();
 	});
 });
 
