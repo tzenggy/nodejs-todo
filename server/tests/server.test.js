f8a0b367ab026ef6160cb6897da217eb717a9278
Zack Tzeng <zack.tzeng@gmail.com>
2018-02-15 15:06:00 -0600

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -254,6 +254,7 @@ describe('POST /users', () => {
 					expect(user.password).not.toBe(password);
 					done();
 				})
+				.catch((err) => {done(err)})
 			});
 	});
 
@@ -278,4 +279,53 @@ describe('POST /users', () => {
 			.expect(400)
 			.end(done);
 	});
-})
+});
+
+describe('POST /users/login', () => {
+	it('should login user and return auth token', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password: users[1].password
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.headers['x-auth']).toBeTruthy();
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				User.findById(users[1]._id.toHexString()).then((user) => {
+					expect(user.tokens[0]).toHaveProperty('access', 'auth');
+					expect(user.tokens[0]).toHaveProperty('token', res.headers['x-auth']);
+					done();
+				}).catch ((err) => done(err));
+
+			})
+	});
+
+	it('should reject invalid login', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password: users[1].password + '1'
+			})
+			.expect(400)
+			.expect((res) => {
+				expect(res.headers['x-auth']).toBeFalsy();
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				User.findById(users[1]._id.toHexString()).then((user) => {
+					expect(user.tokens.length).toBe(0);
+					done();
+				}).catch ((err) => done(err));
+
+			})
+	});
+});
